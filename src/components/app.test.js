15c84ed9c85ba981737977007baa92812e0fc79b
Mostafa Shahat <mostafa.shahat2014@gmail.com>
2023-03-03 11:05:30 +0200

    add and remove group

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,3 +1,18 @@
 /**
  * @jest-environment jsdom
  */
+import { Node } from "./app";
+
+describe('add and remove group', () => {
+  it('should add a task to the list', () => {
+    const todoList = new Node();
+    todoList.createItems('Write code');
+    expect(todoList.lists).toHaveLength(1);
+    expect(todoList.lists[0]).toEqual({
+      index: 1,
+      description: 'Write code',
+      completed: false,
+    });
+  });
+  
+});
